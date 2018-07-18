a486c8d59b98f2c85e65a152858b03d5e8ddad70
Ben Wexler <mixedywex@gmail.com>
2018-07-17 22:35:10 -0400

    animation added

--- pokemonSwitch.js
+++ pokemonSwitch.js
@@ -5,4 +5,10 @@ let initiate = () => {
     mainScreenParent.style.display="inline";
     let btn = document.getElementById("start");
     btn.style.display="none";
+    let prev=document.getElementById("prev");
+    prev.style.display="inline";
+    let next=document.getElementById("next");
+    next.style.display="inline";
 }
+
+setTimeout(initiate, 1200)
