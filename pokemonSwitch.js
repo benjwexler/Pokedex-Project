deffbf99319601ab47dd3857b9606934ecffbd68
Ben Wexler <mixedywex@gmail.com>
2018-07-17 21:10:44 -0400

    more Styling

---
+++ pokemonSwitch.js
@@ -0,0 +1,8 @@
+// const multiplyES6 = (x, y) => { return x * y };
+
+let initiate = () => {
+    let mainScreenParent = document.getElementById("mainScreenParent");
+    mainScreenParent.style.display="inline";
+    let btn = document.getElementById("start");
+    btn.style.display="none";
+}
